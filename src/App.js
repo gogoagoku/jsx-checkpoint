90330a3479ee72dbb4f523681971357e17063b4b
graietay <sayadon1984@gmail.com>
2023-11-28 14:53:10 +0100

    jsx v2

--- src/App.js
+++ src/App.js
@@ -1,19 +1,70 @@
+import React from 'react';
+
 import './App.css';
+
+
 import Name from './Name';
-import Price from './Price';
 import Description from './Description';
 import Image from './Image';
+import Price from './Price';
+
+import Card from 'react-bootstrap/Card';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const a= prompt("Enter your first name:");
+
+
+
 function App() {
+
   return (
-    <div className="App">
-     <Name/>
-     <Price/>
-     <Description/>
-     <Image/>
+
+    <div className='show'>
+
+
+
+
+<div>
+
+<Card style={{ width: 300 }}>
+      <Card.Img variant="top"  />< Image />
+      <Card.Body>
+        <Card.Title>< Name /></Card.Title>
+        <Card.Text>
+        < Price />
+        < Description />
+        </Card.Text>
+        
+      </Card.Body>
+    </Card>
     </div>
-    
-  );
- 
+<div>
+
+<Card style={{ width: 300 }}>
+      <Card.Img variant="top"  />< Image />
+      <Card.Body>
+        <Card.Title>< Name /></Card.Title>
+        <Card.Text>
+        < Price />
+        < Description />
+        </Card.Text>
+        
+      </Card.Body>
+    </Card>
+      </div>
+      
+
+
+
+<div className='alert' style={{ width: 500 }} >
+    <h2>hello , {a ? a : "there !"}!</h2>
+    {a && <img src='./1.png' alt=''/>}
+    </div>
+
+
+
+    </div>
+  )
 }
- 
-export default App;
+
+export default App
